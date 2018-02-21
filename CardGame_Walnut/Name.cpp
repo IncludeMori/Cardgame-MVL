f8f906112802051072424425075b7b85f828dcb4
Ombromon <volkenandt.mo@web.de>
2018-02-21 22:24:26 +0100

    added: player names

--- CardGame_Walnut/Name.cpp
+++ CardGame_Walnut/Name.cpp
@@ -1,11 +1,13 @@
 #include "Name.hpp"
 
 #include "gScreenSize.hpp"
+#include "gRenderer.hpp"
 
 Name::Name()
 {
 	CurrentValue = "Player_Unkown";
 	loadFromRenderedText(CurrentValue);
+	mBackground.loadFromFile("Data/Hero_Interface/name_background.png");
 }
 
 
@@ -19,15 +21,15 @@ void Name::init(Who o,const std::string &name)
 	CurrentValue = name;
 	loadFromRenderedText(CurrentValue);
 	
-
-
 	if (o == PLAYER)
 	{
-		setPos(700, SCREEN_HEIGHT - 150);
+		setPos(700-10, SCREEN_HEIGHT - 163);
+		mBackground.setPos(700-mBackground.getWidth()+mWidth+27, SCREEN_HEIGHT - 150 - mHeight/2-3);
 	}
 	else
 	{
-		setPos(700, 240);
+		setPos(700-10, 230);
+		mBackground.setPos(700-mBackground.getWidth()+mWidth+27-12, 240-mHeight/2-3);
 	}
 }
 void Name::setName(const std::string &name)
@@ -35,4 +37,22 @@ void Name::setName(const std::string &name)
 
 	CurrentValue = name;
 	loadFromRenderedText(CurrentValue);
+}
+
+void Name::render(SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip)
+{
+	mBackground.render();
+	//Set rendering space and render to screen
+	SDL_Rect renderQuad = { mPosX,mPosY, mWidth, mHeight };
+
+
+	if (clip != NULL)
+	{
+		renderQuad.w = clip->w;
+		renderQuad.h = clip->h;
+	}
+
+
+	SDL_RenderCopyEx(gRenderer, mTexture, clip, &renderQuad, angle, center, flip); // renders texture to screen
+
 }
--- CardGame_Walnut/Name.hpp
+++ CardGame_Walnut/Name.hpp
@@ -4,6 +4,7 @@
 
 #include "TTF_Text.hpp"
 #include "Who.hpp"
+#include "DefaultTexture.hpp"
 
 class Name : public TTF_Text
 {
@@ -14,6 +15,9 @@ public:
 	void init(Who o,const std::string &name);
 	void setName(const std::string &name);
 
+	void render(SDL_Rect* clip = nullptr, double angle = 0.0, SDL_Point* center = nullptr, SDL_RendererFlip flip = SDL_FLIP_NONE);
+
 private:
+	DefaultTexture mBackground;
 };
 
